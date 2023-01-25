6417ac9fd1f6ebcb16854b2e7ad2957a05fe1177
dev-raufhossain <freelancerraufhossain@gmail.com>
2023-01-25 22:04:09 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+var naveMenu = document.getElementById('navmenu');
+function openMenu(){
+navmenu.style.right ="0";
+}
+function hideMenu(){
+    naveMenu.style.right ="-300px";
+    }
+    /* HEDER END */
